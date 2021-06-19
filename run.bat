57c22bd30fe9043692719470a8c3467ac1000841
Oliver Weichhold <oliver@weichhold.com>
2021-06-19 19:24:23 +0200

    WIP

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-docker run -it -p 5888:5888 -v d:\blockchain\vertcoin-test:/data -e "EXTRA_OPTIONS=-prune=1000" coinfoundry/vertcoin-public-testnet
+docker run -it -d -p 5888:5888 -v d:\blockchain\vertcoin-test:/data -e "EXTRA_OPTIONS=-prune=1000" coinfoundry/vertcoin-public-testnet
